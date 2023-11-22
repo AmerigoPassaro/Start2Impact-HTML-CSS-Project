f335d486680d5af203ea0e61f5edebd11285381f
AmerigoPassaro <110347819+AmerigoPassaro@users.noreply.github.com>
2023-11-22 12:08:00 +0100

    Caricamento file
    Caricamento file JS in COMPOENTS: BioText
    
    Sostituzione file JS in COMPOENTS: Avatar
    
    Sostituzione file HTML in ROOT: Index

--- assets/js/components/avatar.js
+++ assets/js/components/avatar.js
@@ -32,23 +32,3 @@ function avatar(){
     }
 
 }
-
-//   <section>
-    //     <h2>
-    //       <b>Bio</b>graphy
-    //     </h2>
-    //     <article id="bio">
-    //       <div>
-    //         <p class="main-question">
-    //           Chi sono?
-    //         </p>
-    //         <p class="main-text">
-    //           Mi presento, sono <strong>Amerigo</strong> e sono un <strong>Graphic Deisgner</strong> e un <strong>Frontend Developer</strong>.<br>
-    //           Nato a <strong>Caltanissetta</strong>, una piccola citt&agrave; dell'entroterra siciliano, ho conseguito il diploma presso l'ITC "Mario Rapisardi" nell'anno 2013.<br>
-    //           Dopo una lunga esperienza come graphic designer all'interno di un'azienda locale, ho deciso di focalizzarmi su una mia vecchia passio, lo sviluppo web.<br>
-    //           Per questo ho intrapreso nel 2022 il percorso di sviluppo frontend presso <strong>Start2Impact</strong> e il percorso di ingegneria informatica presso l'universit&agrave; telematica <strong>Universitas Mercatorum</strong>.<br>
-    //         </p>
-
-    //       </div>
-    //     </article>
-    //   </section>
