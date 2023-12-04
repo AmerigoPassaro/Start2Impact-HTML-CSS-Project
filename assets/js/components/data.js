7d769cc3e4820238728dcccaf15564571176a56f
AmerigoPassaro <110347819+AmerigoPassaro@users.noreply.github.com>
2023-12-04 19:32:53 +0100

    Modifica
    Modifica file JS in COMPONENTS: Data
    
    Modifica file CSS in CSS: Bio, Main, Style
    
    Modifica file HTML in ROOT: Index
    
    Caricamento file CSS in CSS: Skill

--- assets/js/components/data.js
+++ assets/js/components/data.js
@@ -3,21 +3,28 @@ let brand = "<b>Amerigo</b>Passaro";
 let menuItem = ["Home", "Contacts", "Mode"];
 let menuFunc = ["goToHome()", "goToContacts()", "changeMode()"];
 
+// Main
+let sections = ["Biography", "DevelopmentSkills", "PersonalPortfolio"];
+let questions = ["Chi sono?", "Cosa so fare?", "Vi mostro qualcosa?"];
 
-let socials = ["https://www.instagram.com/amerigo_passaro/", "https://www.linkedin.com/in/amerigo-passaro-280162152/", "https://github.com/AmerigoPassaro"];
-let iconS = ["assets/img/social/ig.svg", "assets/img/social/in.svg", "assets/img/social/git.svg"];
-
+// Bio
 let textBio = `Mi presento, sono <strong>Amerigo</strong> e sono un <strong>Graphic Deisgner</strong> e un <strong>Frontend Developer</strong>.<br>
 Nato a <strong>Caltanissetta</strong>, una piccola citt&agrave; dell'entroterra siciliano, ho conseguito il diploma presso l'ITC "Mario Rapisardi" nell'anno 2013.<br>
 Dopo una lunga esperienza come graphic designer all'interno di un'azienda locale, ho deciso di focalizzarmi su una mia vecchia passio, lo sviluppo web.<br>
 Per questo ho intrapreso nel 2022 il percorso di sviluppo frontend presso <strong>Start2Impact</strong> e il percorso di ingegneria informatica presso l'universit&agrave; telematica <strong>Universitas Mercatorum</strong>.<br>`;
 
+
+// Social
+let socials = ["https://www.linkedin.com/in/amerigo-passaro-280162152/", "https://github.com/AmerigoPassaro"];
+let iconS = ["assets/img/social/in.svg", "assets/img/social/git.svg"];
+
+
+
 let copy = "Amerigo Passaro &copy";
 
 
 
-let sections = ["Biography", "DevelopmentSkills", "PersonalPortfolio"];
-let questions = ["Chi sono?", "Cosa so fare?", "Vi mostro qualcosa?"];
+
 let skills = ["html", "css", "javascript", "react"];
 
 
