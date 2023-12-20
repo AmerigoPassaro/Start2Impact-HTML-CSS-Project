9f64070f69e04fbb2faf9634644a2d99b0037214
AmerigoPassaro <110347819+AmerigoPassaro@users.noreply.github.com>
2023-12-20 23:57:33 +0100

    Modifica
    Modifica file JS in Components: Bio e Data
    
    Modifica file CSS in CSS: Bio e Main

--- assets/js/components/bio.js
+++ assets/js/components/bio.js
@@ -5,11 +5,23 @@ function bio(){
 
     // Creazione avatar
     let bio = document.createElement("p");
+    
     // Inserimento attributo
     bio.setAttribute("class", "main-text");
     // Inserimento contneuto
     bio.innerHTML = textBio;
     // Inserimento nella section
     document.querySelector("#container-Biography").querySelector(".main-question").after(bio);
+    for(let i = 1; i < sections.length; i++){
+        // Creazione elemento
+        let navSection = document.createElement("button");
+        // Inserimento attributi
+        navSection.setAttribute("id", `${sections[i]}`);
+        navSection.setAttribute("class", "nav-section-item");
+        // Inserimento valore
+        navSection.innerHTML = sections[i];
+        // Inserimento nella pagina
+        document.querySelector("#container-Biography").querySelector(".main-text").append(navSection);
+    }
 
 }
